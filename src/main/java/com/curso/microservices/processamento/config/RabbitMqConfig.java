a7fdae13a752b6cc9a5423b9c0f852a40a6d1c42
victornext <victorbrs517@gmail.com>
2025-09-15 15:18:48 -0300

    pasta consumer

--- src/main/java/com/curso/microservices/processamento/config/RabbitMqConfig.java
+++ src/main/java/com/curso/microservices/processamento/config/RabbitMqConfig.java
@@ -1,4 +1,19 @@
 package com.curso.microservices.processamento.config;
 
+import org.springframework.amqp.core.Queue;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class RabbitMqConfig {
+
+    @Value("${broker.queue.processamento.name}")
+    private String queue;
+
+
+    @Bean
+    public Queue queue(){
+        return new Queue(queue, true);
+    }
 }
--- src/main/java/com/curso/microservices/processamento/consumer/ProcessamentoConsumer.java
+++ src/main/java/com/curso/microservices/processamento/consumer/ProcessamentoConsumer.java
@@ -1,4 +1,14 @@
 package com.curso.microservices.processamento.consumer;
 
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Component;
+
+@Component
 public class ProcessamentoConsumer {
+
+    @RabbitListener(queues = "${broker.queue.processamento.name}")
+    public void listenerProcessamentoQueu(@Payload String descricao) {
+        System.out.print(descricao);
+    }
 }
